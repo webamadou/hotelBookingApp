04e8e7f53cedd567f26cedd86a79fa43bac17fb3
amadou_mac <paab26@live.fr>
2019-06-16 02:57:57 +0000

    create model booking  and migration

--- database/migrations/2019_06_16_015437_add_customer_fields_touser.php
+++ database/migrations/2019_06_16_015437_add_customer_fields_touser.php
@@ -9,11 +9,26 @@ class AddCustomerFieldsTouser extends Migration
     /**
      * Run the migrations.
      *
+     * THE USER MIGRATION IS USE TO SET CUSTOMER DATA
+     *
      * @return void
      */
     public function up()
     {
         //
+        Schema::table('users', function (Blueprint $table) {
+            //WE NEED TO DELETE THE COLUMN 'name' AND REPLACE IT WITH WITH firstname AND lastname.
+            if(Schema::hasColumn('users','name')){
+                $table->dropColumn('name');
+            }
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->mediumText('address');
+            $table->mediumText('city');
+            $table->mediumText('country');
+            $table->mediumText('phone')->nullable(true)->default(null);
+            $table->mediumText('fax')->nullable(true)->default(null);
+        });
     }
 
     /**
@@ -23,6 +38,17 @@ class AddCustomerFieldsTouser extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('users', function(Blueprint $table){
+            if(!Schema::hasColumn('users','name')){
+                $table->string('name');
+            }
+            $table->dropColumn('first_name');
+            $table->dropColumn('last_name');
+            $table->dropColumn('address');
+            $table->dropColumn('city');
+            $table->dropColumn('country');
+            $table->dropColumn('phone');
+            $table->dropColumn('fax');
+        });
     }
 }
---
+++ database/migrations/2019_06_16_023511_create_bookings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('room_id');
+            $table->dateTime('date_start');
+            $table->dateTime('date_end');
+            $table->unsignedInteger(user_id);
+            $table->unsignedInteger(price_id);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
